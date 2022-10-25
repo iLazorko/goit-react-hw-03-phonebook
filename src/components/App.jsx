7bf408bd5ef7666ab5578003900457487b3b0460
Inna Lazorko <inna-h@i.ua>
2022-10-25 19:57:07 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,66 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { ContactsForm } from './contactsForm/ContactsForm';
+import { Filter } from './filter/Filter';
+import { ContactList } from './contactList/ContactList';
+import { Wrapper, Title, Heading } from './App.styled';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+
+    // для тестування фільтра
+    //  contacts: [
+    //     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    //     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    //     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    //     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    //   ],
+  };
+
+  handleChangeFilter = evt => {
+    const { name, value } = evt.target;
+    this.setState({ [name]: value });
+  };
+
+  contactsCatalog = newContact => {
+    if (
+      this.state.contacts.some(
+        contactPerson =>
+          contactPerson.name.toLowerCase() === newContact.name.toLowerCase()
+      )
+    ) {
+      alert(`${newContact.name} is already in contacts!`);
+      return;
+    }
+
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, newContact],
+    }));
+  };
+
+  deleteContact = id => {
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts.filter(contact => contact.id !== id)],
+    }));
+  };
+
+  render() {
+    const { filter, contacts } = this.state;
+    return (
+      <>
+        <Wrapper>
+          <Heading>Phonebook</Heading>
+          <ContactsForm contactsCatalog={this.contactsCatalog} />
+          <Title>Contacts</Title>
+          <Filter onChange={this.handleChangeFilter} />
+          <ContactList
+            contacts={contacts}
+            filter={filter}
+            deleteContact={this.deleteContact}
+          />
+        </Wrapper>
+      </>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import { App } from 'components/App.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -8,3 +8,11 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>
 );
+
+// добавь хранение контактов телефонной книги в localStorage.
+// Используй методы жизненного цикла.
+
+// При добавлении и удалении контакта, контакты сохраняются в локальное
+// хранилище.
+// При загрузке приложения, контакты, если таковые есть, считываются из
+// локального хранилища и записываются в состояние.
